bfea2094e5d827d0fb7a9a93a7e2c3f27670145d
Matias Roccazzella <matias.a.re@hotmail.com>
2022-09-22 21:03:15 -0300

    Desafio 2

--- desafio2.js
+++ desafio2.js
@@ -12,37 +12,79 @@ class Container{
         this.route = route
     }
     //METODOS
-    async creacion(){
+    
+    async getAll(){
         try{
-            await fs.writeFile(this.route,'')
+            const contenido = await fs.readFile(this.route, "utf8")
+            const elementos = JSON.parse(contenido)
+            return elementos
         }
         catch(error){
-            throw new Error (`El archivo no pudo crearse, error: ${error}`)
+            if(error.code === "ENOENT"){
+                await fs.writeFile(this.route,JSON.stringify([],null,3))
+                return []
+            }
+            throw new Error (`${error.message}`)
+            
         }
+        
     }
-    
     async save(object){
+        
         try {
-            let contenido = await fs.readFile(this.route,"utf-8")
-            if(contenido.includes('id')){
-                let array = contenido.split('id')
-                let id = array.length
-                object.id = id + 1
-            }
-            else{
-                object.id = 1
-            }
-            await fs.appendFile(this.route,JSON.stringify(object))
+            const elementos = await this.getAll()
+            const id = elementos.length === 0 ? 1 : elementos[elementos.length - 1].id + 1
+            object.id = id
+            elementos.push(object)
+            await fs.writeFile(this.route,JSON.stringify(elementos,null,3))
+            return object.id
 
         }
         catch(error){
-            throw new Error (`El archivo no pudo manipularse, error: ${error}`)
+            throw new Error (`El archivo no pudo manipularse, error: ${error.message}`)
         }
+    }
+    async getById(number){
+        try {
+            const elementos = await this.getAll()
+            let elemento = elementos.find((element) => element.id == number)
+            if(!elemento){
+                return null
+            }
+            return {elemento}
+        }
+        catch(error){
+            throw new Error (`El archivo no pudo manipularse, error: ${error.message}`)
+        }
+    }
+    async deleteById(number){
+        try {
+            const elementos = await this.getAll()
+            let elemento = elementos.find((element) => element.id == number)
+            if (!elemento) return 'element not found'
+            const elementosFiltrados = elementos.filter((element) => element.id != number)
+            await fs.writeFile(this.route,JSON.stringify(elementosFiltrados,null,3))
+        }
+        catch(error){
+            throw new Error (`El archivo no pudo manipularse, error: ${error.message}`)
+        }
+    }
+    async deleteAll(){
+        try {
+            await fs.unlink(this.route)
+        } catch (error) {
+            throw new Error(`Error al Eliminar: ${error.message}`)
+        } 
     }
 }
-const archivo = new Container('./archivo.txt')
-//const producto1 = new Productos('Fideos',200)
-//const producto2 =new Productos('arroz',250)
-archivo.creacion()
-archivo.save({titulo: 'fideos', precio: 200})
+const archivo = new Container('./archivo.json')
 
+//archivo.getAll().then((data) => console.log({data})).catch((error) => console.log({error}))
+
+//archivo.save({titulo: 'fideos', precio: 350}).then((data) => console.log({data})).catch((error) => console.log({error}))
+
+//archivo.getById(21).then((data) => console.log({data})).catch((error) => console.log({error}))
+
+//archivo.deleteById(19).then((data) => console.log({data})).catch((error) => console.log({error}))
+
+//archivo.deleteAll()
